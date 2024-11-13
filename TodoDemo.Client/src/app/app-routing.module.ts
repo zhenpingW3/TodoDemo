048c93d503df7221a7b43621e47e93b759dcd9d1
Benson Wang <benson.wang@moodys.com>
2024-11-13 11:16:48 -0500

    show todos and health check

--- TodoDemo.Client/src/app/app-routing.module.ts
+++ TodoDemo.Client/src/app/app-routing.module.ts
@@ -8,8 +8,9 @@ import { TodoListComponent } from './todo-list/todo-list.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  //Lazy Loading
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
-  { path: 'nav-menu', component: NavMenuComponent  },
+  { path: 'nav-menu', component: NavMenuComponent },
   { path: 'jqxgrid-demo', component: HomeComponent },
   { path: 'fetch-data', component: FetchDataComponent },
   { path: 'health-check', component: HealthCheckComponent },
